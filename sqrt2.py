fb5f4ac2a9e3f8b8223f351a057f05d2d03c66fd
robin93 <robinsinghiitd@gmail.com>
2015-08-07 20:16:59 +0530

    added comment for input of functions

--- sqrt2.py
+++ sqrt2.py
@@ -1,6 +1,16 @@
 #program to calculate the square root
 include
 
+#funtion to calculate the Arithmetic mean of 2 numbers
+
+#function to calculate the Geometric mean of 2 numbers
+
+
+#function to compare number against a value and give true/false as output depending on the equality
+
+
+
+#function which will take 2 inputs, the number to calculate sqrt of and precision
 def function():
 	pass
 
@@ -8,4 +18,4 @@ def function():
 	while error < 0.00001:
 		pass
 
-		another set of changes to the file 
+		 
